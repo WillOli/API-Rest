593a8419a099c4952376e3d37dd90f46425df552
WillOli <william.ika@gmail.com>
2023-09-27 08:38:45 -0300

    Criando usuario e repositorio usuario

--- src/main/java/com/willdev/controller/welcomeSpring.java
+++ src/main/java/com/willdev/controller/welcomeSpring.java
@@ -1,10 +1,11 @@
 package com.willdev.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class welcomeSpring {
-
+    @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to a Spring Boot Rest API";
     }
